9c1be253ec14488b4137983af086e895d5ab75f3
wangjianzhou <tinytongtong@163.com>
2019-08-06 15:26:53 +0800

    显式Intent启动其他应用的Activity。

---
+++ app/src/main/java/com/tinytongtong/dividerviewdemo/TestImplicitIntentActivity.java
@@ -0,0 +1,20 @@
+package com.tinytongtong.dividerviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * @Description: 隐式Intent启动
+ *
+ * @Author wangjianzhou@qding.me
+ * @Date 2019-08-06 10:54
+ * @Version
+ */
+public class TestImplicitIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_implicit_intent);
+    }
+}
